e92885add3cc3332a2a8f9fdef5ce51c62c54cee
mapatil1 <56800032+mapatil1@users.noreply.github.com>
2020-01-23 18:17:31 +0530

    Update s4sdk-pipeline.groovy

--- s4sdk-pipeline.groovy
+++ s4sdk-pipeline.groovy
@@ -41,10 +41,10 @@ pipeline {
                     when { expression { commonPipelineEnvironment.configuration.runStage.BACKEND_UNIT_TESTS } }
                     steps { stageUnitTests script: this }
                 }
-                stage("Backend Integration Tests") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.BACKEND_INTEGRATION_TESTS } }
-                    steps { stageBackendIntegrationTests script: this }
-                }
+            //    stage("Backend Integration Tests") {
+              //      when { expression { commonPipelineEnvironment.configuration.runStage.BACKEND_INTEGRATION_TESTS } }
+                //    steps { stageBackendIntegrationTests script: this }
+                //}
                 stage("Frontend Integration Tests") {
                     when { expression { commonPipelineEnvironment.configuration.runStage.FRONTEND_INTEGRATION_TESTS } }
                     steps { stageFrontendIntegrationTests script: this }
@@ -53,10 +53,10 @@ pipeline {
                     when { expression { commonPipelineEnvironment.configuration.runStage.FRONTEND_UNIT_TESTS } }
                     steps { stageFrontendUnitTests script: this }
                 }
-                stage("NPM Dependency Audit") {
-                    when { expression { commonPipelineEnvironment.configuration.runStage.NPM_AUDIT } }
-                    steps { stageNpmAudit script: this }
-                }
+               // stage("NPM Dependency Audit") {
+                 //   when { expression { commonPipelineEnvironment.configuration.runStage.NPM_AUDIT } }
+                   // steps { stageNpmAudit script: this }
+               // }
             }
         }
 
